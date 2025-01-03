44c916a5d78f9239fe9f6333e037750869ddd9f2
Tasya <rabchinskayavita811@gmail.com>
2025-01-04 00:29:35 +0100

    Implemented weather fetching functionality and changed the main page design

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,24 @@
+import requests # is used for post and get requests
 from django.shortcuts import render
 
+#this function is processing requests on a home page of an app
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = 'c3efdac01664d4464d1f614b2a2ec6fa'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metrics&appid=' + appid
+
+    city = 'London'
+    res = requests.get(url.format(city)).json() # method get sends get-request, format is used for substitution values into {}, json() converts json format into a dictionary
+
+    #for getting info from the set(city, temperature, weather item)
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'item': res["weather"][0]["icon"]
+    }
+
+    context = {
+        'info': city_info
+    }
+    #print(res.text)
+
+    return render(request, 'weather/index.html', context)
